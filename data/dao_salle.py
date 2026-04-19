e425b1ebcdbfbd0cec6fc0754d2c9c16e7725af8
Patricia-Nguemedzi <nguemanu40@gmail.com>
2026-04-19 14:08:27 -0400

    implementation de la methode insert_salle pour l'ajout en base de données.

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -5,19 +5,40 @@ class DataSalle:
     def get_connection(self):
 
         try:
-            with open ("./data/config.json", "r", encoding="Utf-8") as f:
+            with open ("data/config.json", "r", encoding="Utf-8") as f:
                 config = json.load(f)
 
-                connection = mysql.connector.connect(
-                    host=config["host"],
+            connection = mysql.connector.connect(
+                host=config["host"],
                 user=config["user"],
                 password=config["password"],
                 database=config["database"]
-                )
-                return connection
+            )
+            return connection
         except mysql.connector.Error as error:
             print(f"erreur de connexion : {error}")
             return None
+
+    def insert_salle(self, salle):
+        connection = self.get_connection()
+        if connection:
+            try:
+                cursor = connection.cursor()
+                cursor.execute(
+                "INSERT INTO salle (code, description,categorie, capacite) values (%s, %s, %s, %s)",
+                (salle.code, salle.description, salle.categorie, salle.capacite)
+                )
+
+                connection.commit()
+                print(f"La salle {salle.code} a été ajoutée avec succès.")
+
+            except mysql.connector.Error as err:
+                print(f"Erreur lors de l'insertion : {err}")
+
+            finally:
+                cursor.close()
+                connection.close()
+
 
 
 
