eab607e137300b9fdbda21b9644b2d09c2e2a271
Patricia-Nguemedzi <nguemanu40@gmail.com>
2026-04-19 22:47:48 -0400

    test de la supression d'une sale à la base de donnée

--- models/salle.py
+++ models/salle.py
@@ -1,16 +1,16 @@
 class Salle:
-    def __init__(self, code, description, categorie, capacite):
-        self.code = code
-        self.description = description
+    def __init__(self, codes, descriptions, categorie, capacite):
+        self.codes = codes
+        self.descriptions = descriptions
         self.categorie = categorie
         self.capacite = capacite
 
     def afficher_infos(self):
         print("-" * 50)
-        print(f"Details complets de la salle de classe {self.code}")
+        print(f"Details complets de la salle de classe {self.codes}")
         print("-" * 50)
-        print(f"code        : {self.code}")
-        print(f"description : {self.description}")
+        print(f"code        : {self.codes}")
+        print(f"description : {self.descriptions}")
         print(f"categorie   : {self.categorie}")
         print(f"capacite    : {self.capacite} personnes.")
         print("-" * 50)
