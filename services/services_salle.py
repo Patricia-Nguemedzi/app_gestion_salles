9f9b3308daa14a1247f3099154ee657042a26d90
Patricia-Nguemedzi <nguemanu40@gmail.com>
2026-04-20 08:11:36 -0400

    implementation de la methode  ajouter_salle(salle)

--- services/services_salle.py
+++ services/services_salle.py
@@ -6,3 +6,12 @@ class ServiceSalle:
     def __init__(self):
         self.dao_salle = DataSalle()
 
+    def ajouter_salle(self, salle):
+        if not salle.codes or not salle.descriptions or not salle.categorie:
+            return False, "Erreur : Toutes les informations sont obligatoires."
+
+        if salle.capacite < 1:
+            return False, "Erreur : La capacité doit être d'au moins 1 personne."
+
+        self.dao_salle.insert_salle(salle)
+        return True, f"La salle {salle.codes} a été ajoutée avec succès."
